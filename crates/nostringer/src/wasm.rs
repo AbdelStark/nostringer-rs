cf72dda269b757213bea5f66d10f54ad1e415a0e
Abdel @ StarkWare <abdelhamid.bakhta@gmail.com>
2025-03-28 17:53:22 +0100

    Fix WASM build issues and add compiled WASM package for web example

--- crates/nostringer/src/wasm.rs
+++ crates/nostringer/src/wasm.rs
@@ -12,7 +12,6 @@ use crate::{
 pub fn set_panic_hook() {
     // When the `console_error_panic_hook` feature is enabled, we can call the
     // `set_panic_hook` function to get better error messages if a panic occurs.
-    #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
 }
 
